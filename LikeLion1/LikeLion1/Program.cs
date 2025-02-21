0a28f3d622d384834e2d411e7ae80fd377b4a4b5
donghwiSon <ehdgnlgk@gmail.com>
2025-02-21 10:44:20 +0900

    character

--- LikeLion1/LikeLion1/Program.cs
+++ LikeLion1/LikeLion1/Program.cs
@@ -10,9 +10,12 @@ namespace LikeLion1
     {
         static void Main(string[] args)
         {
-            int age;
-            age = 25;
-            Console.WriteLine(age);
+            //캐릭터 
+            //hp : 100
+            //att : 56.7
+            //캐릭터이름 : ??
+            //등급 : S
+            
         }
     }
 }
