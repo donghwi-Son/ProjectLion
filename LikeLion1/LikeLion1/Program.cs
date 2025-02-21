cf65f7510c90d3d1bfbe4a027e3b15ae04e20c13
donghwiSon <ehdgnlgk@gmail.com>
2025-02-21 13:02:07 +0900

    lostark

--- LikeLion1/LikeLion1/Program.cs
+++ LikeLion1/LikeLion1/Program.cs
@@ -10,12 +10,22 @@ namespace LikeLion1
     {
         static void Main(string[] args)
         {
-            //캐릭터 
-            //hp : 100
-            //att : 56.7
-            //캐릭터이름 : ??
-            //등급 : S
-            
+            int att = 16755;
+            int maxhp = 78103;
+            int crit = 36;
+            int special = 1017;
+            int overpower = 41;
+            int speed = 611;
+            int patience = 22;
+            int skill = 39;
+            Console.WriteLine("공격력: " + att);
+            Console.WriteLine("최대 생명력: " + maxhp);
+            Console.WriteLine("치명: " + crit);
+            Console.WriteLine("특화: " + special);
+            Console.WriteLine("제압: " + overpower);
+            Console.WriteLine("신속: " + speed);
+            Console.WriteLine("인내: " + patience);
+            Console.WriteLine("숙련: " + skill);
         }
     }
 }
