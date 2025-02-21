b9f88d5c8b7f3e6cca1e5a5e8be2ac5c139ba266
donghwiSon <ehdgnlgk@gmail.com>
2025-02-21 16:02:26 +0900

    day1last

--- LikeLion1/LikeLion1/Program.cs
+++ LikeLion1/LikeLion1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LikeLion1
@@ -10,22 +11,38 @@ namespace LikeLion1
     {
         static void Main(string[] args)
         {
-            int att = 16755;
-            int maxhp = 78103;
-            int crit = 36;
-            int special = 1017;
-            int overpower = 41;
-            int speed = 611;
-            int patience = 22;
-            int skill = 39;
-            Console.WriteLine("공격력: " + att);
-            Console.WriteLine("최대 생명력: " + maxhp);
-            Console.WriteLine("치명: " + crit);
-            Console.WriteLine("특화: " + special);
-            Console.WriteLine("제압: " + overpower);
-            Console.WriteLine("신속: " + speed);
-            Console.WriteLine("인내: " + patience);
-            Console.WriteLine("숙련: " + skill);
+            Console.Write("루인 스킬 피해 입력 : ");
+            float sd = float.Parse(Console.ReadLine());
+            Console.Write("카드 게이지 획득량 입력 : ");
+            float gauge = float.Parse(Console.ReadLine());
+            Console.Write("각성기 피해 입력 : ");
+            float ultd = float.Parse(Console.ReadLine());
+            Console.Write("최대 마나 입력 : ");
+            float maxmp = float.Parse(Console.ReadLine());
+            Console.Write("전투 중 마나 회복량 입력 : ");
+            float battlemp = float.Parse(Console.ReadLine());
+            Console.Write("비전투 중 마나 회복량 입력 : ");
+            float nonbatmp = float.Parse(Console.ReadLine());
+            Console.Write("이동 속도 입력 : ");
+            float mvspd = float.Parse(Console.ReadLine());
+            Console.Write("탈 것 속도 입력 : ");
+            float ridespd = float.Parse(Console.ReadLine());
+            Console.Write("운반 속도 입력 : ");
+            float transspd = float.Parse(Console.ReadLine());
+            Console.Write("스킬 재사용 대기시간 감소 입력 : ");
+            float cooldown = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"루인 스킬 피해  : {sd}%");
+            Console.WriteLine($"카드 게이지 획득량  : {gauge}%");
+            Console.WriteLine($"각성기 피해  : {ultd}%");
+            Console.WriteLine("최대 마나  : " + maxmp);
+            Console.WriteLine("전투 중 마나 회복량  : " + battlemp);
+            Console.WriteLine("비전투 중 마나 회복량 : " + nonbatmp);
+            Console.WriteLine($"이동 속도  : {mvspd}%");
+            Console.WriteLine($"탈 것 속도  : {ridespd}%");
+            Console.WriteLine($"운반 속도  : {transspd}%");
+            Console.WriteLine($"스킬 재사용 대기시간 감소  : {cooldown}%");
         }
     }
 }
